1caef42a2347e791812e6dfd471d1f0fbca894e5
JuanTader500 <j.munoz12@utp.edu.co>
2025-10-28 12:44:38 -0500

    Hcaemos un primer commit de nuestro poyecto donde se crea lo mas basico la clase de propietario y la clase mascota

---
+++ src/main/java/com/mycompany/finalproyectpoo/Mascota.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.finalproyectpoo;
+
+/**
+ *
+ * @author juana
+ */
+public class Mascota {
+    
+    private int id_mascota;
+    private String nombre;
+    private String especie;
+    private String raza;
+    private String sexo;
+    private int edad;
+    private double peso;
+    private String color;
+    private Propietario propietario;
+    
+    //Generamos el constructor de nuestra calse Mascota
+
+    public Mascota(int id_mascota, String nombre, String especie, String raza, String sexo, int edad, double peso, String color, Propietario propietario) {
+        this.id_mascota = id_mascota;
+        this.nombre = nombre;
+        this.especie = especie;
+        this.raza = raza;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.peso = peso;
+        this.color = color;
+        this.propietario = propietario;
+    }
+    
+    //Generamos los setters de nuestra clase
+
+    public void setId_mascota(int id_mascota) {
+        this.id_mascota = id_mascota;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPropietario(Propietario propietario) {
+        this.propietario = propietario;
+    }
+    
+    //Generamos los getters de nuestra clase 
+
+    public int getId_mascota() {
+        return id_mascota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Propietario getPropietario() {
+        return propietario;
+    }
+
+    @Override
+    public String toString() {
+        return "Mascota{" + "id_mascota=" + id_mascota + ", nombre=" + nombre +
+                ", especie=" + especie + ", raza=" + raza + ", sexo=" + sexo +
+                ", edad=" + edad + ", peso=" + peso + ", color=" + color +
+                ", propietario=" + propietario + '}';
+    }
+    
+    
+}
+
---
+++ src/main/java/com/mycompany/finalproyectpoo/Propietario.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.finalproyectpoo;
+
+/**
+ *
+ * @author juana
+ */
+public class Propietario {
+    
+    private int id;
+    private String gmail;
+    private String telefono;
+    private String direccion;
+    private String nombre;
+
+    
+    //generamos el constructor de nuestra clase 
+    public Propietario(int id, String gmail, String telefono, String direccion, String nombre) {
+        this.id = id;
+        this.gmail = gmail;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.nombre = nombre;
+    }
+    
+    //Generamos los setters de nuestra clase
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //Generamos los getters
+    public int getId() {
+        return id;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    //Para poder hacer la relacion de propietario y mascota en la interfas grafica al crear 
+    //la mascota(Desplegar una lista de propietarios y seleccionar)
+    
+    @Override
+    public String toString() {
+        return "Propietario{" + "id=" + id + ", gmail=" + gmail +
+                ", telefono=" + telefono + ", direccion=" + direccion +
+                ", nombre=" + nombre + '}';
+    }
+     
+}
