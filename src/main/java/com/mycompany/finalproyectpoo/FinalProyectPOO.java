c7b8cb56d22c502874cd85f62ca48e6980b10009
JuanTader500 <j.munoz12@utp.edu.co>
2025-10-29 14:14:28 -0500

    En este commit se agrega en un inicio lo que son las interfases de los usuarios donde se crearon con el tipo de archivo JFrame, se hizo un inicio de las interfases prinsipales y una primera conexion con la base de datos unicamente con la clase veterinario conectado con interfas veterinario. se hizo una prueba de la conexion con la base de datos con exito

--- src/main/java/com/mycompany/finalproyectpoo/FinalProyectPOO.java
+++ src/main/java/com/mycompany/finalproyectpoo/FinalProyectPOO.java
@@ -11,6 +11,7 @@ package com.mycompany.finalproyectpoo;
 public class FinalProyectPOO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        interfas interfas1 = new interfas();
+        interfas1.setVisible(true);
     }
 }
